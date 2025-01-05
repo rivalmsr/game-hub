8190669106e81483f7697fc306eef1e9d3087729
rivalmusripal <rival@baskit.app>
2025-01-05 11:35:04 +0700

    feat(layout): build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from '@chakra-ui/react'
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
 
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area="nav" bg='orange.500'> Nav </GridItem>
+      <Show above='lg'>
+        <GridItem area="aside" bg='green.500'> Aside </GridItem>
+      </Show>
+      <GridItem area="main" bg='blue.500'> main </GridItem>
+    </Grid>
+
   )
 }
 
