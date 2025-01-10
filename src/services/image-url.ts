2406fb33cd332fc9b392b9e57a8cbb3b962ade57
rivalmusripal <rival@baskit.app>
2025-01-10 22:44:08 +0700

    feat(game card): handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholder from '../assets/no-image-placeholder.webp';
+
 export const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImagePlaceholder;
 
   const index = url.indexOf('media/') + 'media/'.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
