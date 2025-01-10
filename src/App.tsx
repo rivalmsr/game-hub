dd0e6ac35477a7a05ce508a0bc03ecefc99c3273
rivalmusripal <rival@baskit.app>
2025-01-10 11:30:49 +0700

    feat(platform): fitler by platform

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import GameGrid from './components/GameGrid'
 import GenreList from './components/GenreList'
 import Navbar from './components/NavBar'
 import PlatformSelector from './components/PlatformSelector';
+import { Platform } from './hooks/useGames';
 import { Genre } from './hooks/useGenres';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <Grid templateAreas={{
@@ -22,8 +24,8 @@ function App() {
         <GridItem area="aside" paddingX={2}> <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} /> </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)} />
+        <GameGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} />
       </GridItem>
     </Grid>
 
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,16 +1,22 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
 import usePlatforms from "../hooks/usePlatforms";
 
+interface Props {
+  selectedPlatform: Platform | null;
+  onSelectPlatform: (platform: Platform) => void;
+}
 
-function PlatformSelector() {
+
+function PlatformSelector({ selectedPlatform, onSelectPlatform }: Props) {
   const { data } = usePlatforms();
 
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>Platforms</MenuButton>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?.name || 'Platforms'}</MenuButton>
       <MenuList>
-        {data.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
+        {data.map(platform => <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>{platform.name}</MenuItem>)}
       </MenuList>
     </Menu>
   )
