339424fdf3801eddccf6703f205707772c45c9dc
rivalmusripal <rival@baskit.app>
2025-01-11 22:31:57 +0700

    fix(genre): cleaning up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, Heading, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import { getCroppedImageUrl } from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
@@ -16,15 +16,18 @@ function GenreList({ selectedGenre, onSelectGenre }: Props) {
   if (loading) return <GenreListSkeleton />
 
   return (
-    <List>{data.map(genre =>
-      <ListItem key={genre.id} paddingY={1}>
-        <HStack>
-          <Image boxSize="32px" src={getCroppedImageUrl(genre.image_background)} borderRadius={8} />
-          <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} fontSize="lg" variant="link" onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
-        </HStack>
-      </ListItem>
-    )}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={2}>Genres</Heading>
+      <List>{data.map(genre =>
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image boxSize="32px" objectFit="cover" src={getCroppedImageUrl(genre.image_background)} borderRadius={8} />
+            <Button whiteSpace="normal" textAlign="left" fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} fontSize="lg" variant="link" onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
+          </HStack>
+        </ListItem>
+      )}
+      </List>
+    </>
   )
 }
 
