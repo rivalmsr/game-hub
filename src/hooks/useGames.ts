c08d3ebaac7f454dd2b38e306873d8e477b85c97
rivalmusripal <rival@baskit.app>
2025-01-29 06:27:43 +0700

    refactor(types): move all interface to entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,9 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from 'ms';
+import { Game } from "../entities/Game";
 import APIClient from "../services/api-http";
 import { FetchResponse } from "../services/api-http";
 import { GameQuery } from "../store";
-import { Platform } from "./usePlatforms";
-
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-  slug: string;
-  description_raw: string;
-}
 
 const apiClient = new APIClient<Game>('/games');
 
